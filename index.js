d1e1a9543b27a3409b438045e5dd26c9fa2545dd
William <wms274@nyu.edu>
2022-09-11 17:42:37 -0400

    commit tree f7444408eca56d98b54bb9ab459fcbb652e5f91d

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Polygon {
+    constructor(array) {
+        this.array = array
+    }
+    get countSides() {
+        return this.array.length
+    }
+    get perimeter() {
+        let sum = this.array.reduce(function(a, b){
+            return a + b;
+        }, 0);
+        return sum
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.array[0]+this.array[1]>this.array[2] && this.array[0]+this.array[2]>this.array[1] && this.array[1]+this.array[2]>this.array[0]) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if (this.array[0]===this.array[1] && this.array[1]===this.array[2] && this.array[2]===this.array[3]) {
+            return true
+        } else {
+            return false
+        }
+    }
+    get area() {
+        let sideSquare = this.array[0]**2
+        return sideSquare
+    }
+}
